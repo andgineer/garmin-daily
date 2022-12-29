782548ec150fa3e84e53d424cbd3e6b48f779cab
sorokan6 <andrei.sorokin_ext@novartis.com>
2022-12-29 05:12:33 +0100

    pinned min Python version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     install_requires=requirements,
     tests_require=tests_requirements,
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     keywords="Garmin Connect Health Google Sheets",
     classifiers=[
         "Programming Language :: Python :: 3",
